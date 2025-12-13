48326bda2d877dd80d852ee97a91383173f1fca6
juansuarezb <juan.suarez04@epn.edu.ec>
2025-12-13 00:44:40 -0500

    Login screen
    Se creó el diseño de la pantalla de inicio de sesión (`activity_login.xml`) con campos para correo electrónico y contraseña. Se agregaron los textos correspondientes en `strings.xml`.
    
    Cambios adicionales:
    - Se actualizó el ID del layout principal en `activity_main.xml` de `main` a `Home`.
    - Se importó la clase `Toast` en `MainActivity.kt`.

--- app/src/main/java/com/cleansound/cleansound/MainActivity.kt
+++ app/src/main/java/com/cleansound/cleansound/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
@@ -31,7 +32,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
