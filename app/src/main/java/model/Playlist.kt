4bfcd67b8ff6c15c76dd4e8a4a6b7079784b5dbc
juansuarezb <juan.suarez04@epn.edu.ec>
2026-01-24 14:53:29 -0500

    feat: Define data structure for the `Playlist` model
    The `Playlist` class has been converted from an empty class to a data class implementing `Serializable` to represent playlist information within the application.
    
    - **`Playlist.kt`**:
        - Converted to a `data class` with properties for `id`, `name`, `coverImage` (resource ID), and `songCount`.
        - Implemented the `Serializable` interface to allow the model to be passed between components.

--- app/src/main/java/model/Playlist.kt
+++ app/src/main/java/model/Playlist.kt
@@ -1,4 +1,10 @@
 package model
 
-class Playlist {
-}
+import java.io.Serializable
+
+data class Playlist(
+    val id: String,
+    val name: String,
+    val coverImage: Int, // resource ID de la imagen
+    val songCount: Int
+) : Serializable
