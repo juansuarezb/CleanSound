5ac4f7fd0afa62108263dd8193c724f403827e70
juansuarezb <juan.suarez04@epn.edu.ec>
2026-01-25 12:59:43 -0500

    doc/Informe2doBimestre

--- app/src/main/java/com/cleansound/cleansound/controller/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.cleansound.cleansound.controller
-
-const val EXTRA_LOGIN = "EXTRA_LOGIN"
-const val MIN_PASSWORD_LENGTH = 8
-const val LOGIN_KEY = "LOGIN_KEY"
-const val PASSWORD_KEY = "PASSWORD_KEY"
-const val SHAREDINFO_FILENAME = "mySharedInformation.dat"
